f07ea62c829dc63b502f75ae40524974d8c2d9e6
Jakob <Jakob@DESKTOP-I04300C.speedport.ip>
2018-12-16 16:47:00 +0100

    paarungen und so

--- src/sportverein/MainApplicationManageController.java
+++ src/sportverein/MainApplicationManageController.java
@@ -10,48 +10,52 @@ package sportverein;
  * @author rauerjakob
  */
 import java.io.*;
+import java.util.ArrayList;
 
 public class MainApplicationManageController {
- private static final String FootballAssociationFilepath = "FootballAssociationObjectStorage.txt";
-    public static void storeFootballAssociation(Verein[] FootballAssociationArray) {
+
+    private static final String FootballAssociationFilepath = "FootballAssociationObjectStorage.txt";
+
+    public static void storeFootballAssociation(ArrayList<Verein> FootballAssociationArray) {
         try {
-           
+
             File footballAssociationStorageFile = new File(FootballAssociationFilepath);
-            FileOutputStream outputStream = new FileOutputStream(footballAssociationStorageFile, true);
+            FileOutputStream outputStream = new FileOutputStream(footballAssociationStorageFile);
             BufferedOutputStream bos = new BufferedOutputStream(outputStream);
             ObjectOutputStream oos = new ObjectOutputStream(bos);
 
             oos.writeObject(FootballAssociationArray);
             System.out.println("stored");
+
             oos.close();
         } catch (IOException ernjo) {
             System.out.println(ernjo.getMessage());
         }
     }
-    
-    public static void loadFootballAssociatenFile() {
-        try{
-        File FootballAssociationStorageFile = new File(FootballAssociationFilepath);
-        FileInputStream fis = new FileInputStream(FootballAssociationStorageFile);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        ObjectInputStream ois = new ObjectInputStream(bis);
-        
-        Object read = null;
-       
-        while(true) {
+
+    public static ArrayList<Verein> loadFootballAssociatenFile() {
+        ArrayList<Verein> read = null;
+
+        try {
+            File FootballAssociationStorageFile = new File(FootballAssociationFilepath);
+            FileInputStream fis = new FileInputStream(FootballAssociationStorageFile);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            ObjectInputStream ois = new ObjectInputStream(bis);
+
             try {
-                read = ois.readObject();
-                System.out.println(read.toString());
-            } catch(Exception e ) {
-                break;
+
+                read = (ArrayList<Verein>) ois.readObject();
+                for (Verein v : read) {
+                    System.out.println(v.toString());
+                }
+
+            } catch (Exception e) {
+                return null;
             }
-                
-        }
-        
-        
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+        return read;
     }
 }
